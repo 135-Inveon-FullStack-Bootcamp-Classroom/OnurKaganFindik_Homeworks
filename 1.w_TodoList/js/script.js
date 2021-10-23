c56aa19f295f4dd409d5649e4c8a68e7ef2ab92b
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-10-23 03:44:40 +0300

    local storage

--- 1.w_TodoList/js/script.js
+++ 1.w_TodoList/js/script.js
@@ -16,6 +16,10 @@ const form= document.querySelector('form');
 const input= document.querySelector('#txtTaskName');
 const btnDeleteAll= document.querySelector('#btnDeleteAll');
 const taskList= document.querySelector('#task-list');
+let items;
+
+// load items
+loadItems();
 
 
 //call event listeners
@@ -34,34 +38,77 @@ function eventListeners(){
 
 }
 
+function loadItems() {
+    items = getItemsFromLS();
+    items.forEach(function (item) {
+        createItem(item);
+    });
+}
+
+// get items from Local Storage
+function getItemsFromLS(){
+    if(localStorage.getItem('items')===null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+    return items;
+}
+
+// set item to Local Storage
+function setItemToLS(text){
+    items = getItemsFromLS();
+    items.push(text);
+    localStorage.setItem('items',JSON.stringify(items));
+}
+
+// delete item from Local Storage
+function deleteItemFromLS(text){
+    items = getItemsFromLS();
+    items.forEach(function(item,index){
+        if(item === text){
+            items.splice(index,1);   
+        }
+    });
+    localStorage.setItem('items',JSON.stringify(items));
+}
+
+function createItem(text) {
+    // create li
+    const li = document.createElement('li');
+    li.className = 'list-group-item list-group-item-secondary';
+    li.appendChild(document.createTextNode(text));
+
+    // create a
+    const a = document.createElement('a');
+    a.classList = 'delete-item float-right';
+    a.setAttribute('href', '#');
+    a.innerHTML = '<i class="fas fa-times"></i>';
+
+    // add a to li
+    li.appendChild(a);
+
+    // add li to ul
+    taskList.appendChild(li);
+
+}
+
 //add new item
 function addNewItem(e){
     if(input.value ===''){
         alert('Bir görev giriniz.');
     }
 
-    // create li element
-    const li=document.createElement('li');
-    // add class
-    li.className='list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
+     // create item
+     createItem(input.value);
 
-    // create a element
-    const a=document.createElement('a');
-    a.className='delete-item float-right';
-    a.setAttribute('href','#');
-    a.innerHTML='<i class="fas fa-times"></i>';
-
-    // create text node and append to a
-    li.appendChild(a);
-
-    //add li to ul
-    taskList.appendChild(li);
-    
-    //clear input
-    input.value='';
-
-    e.prenventDefault();
+     // save to LS
+     setItemToLS(input.value);
+ 
+     // clear input
+     input.value = '';
+ 
+     e.preventDefault();
 
 }
 
@@ -70,13 +117,23 @@ function deleteItem(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         if(confirm('Görevi silinsin mi?')){
             e.target.parentElement.parentElement.remove();
+
+            // delete item from Local Storage
+            deleteItemFromLS(e.target.parentElement.parentElement.textContent);
+        
         }
     }
+    e.preventDefault();
+        
 }
 
 //delete all items
 function deleteAllItems(e){
     if(confirm('Tüm görevler silinsin mi?')){
-        taskList.innerHTML='';
+        while(taskList.firstChild){
+            taskList.removeChild(taskList.firstChild);
+        }
+        localStorage.clear();
     }
+    e.preventDefault();
 }
