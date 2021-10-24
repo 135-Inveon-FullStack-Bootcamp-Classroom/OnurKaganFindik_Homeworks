5312997f66aa91ed67a97ca9701db193c50cb0a0
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-10-24 21:48:56 +0300

    Readme netlify added

--- 1.w_TodoList/js/script.js
+++ 1.w_TodoList/js/script.js
@@ -12,10 +12,10 @@
  * 5. kategoriler koyulabilir -> spor, iş vs.
  */
 
-const form= document.querySelector('form');
-const input= document.querySelector('#txtTaskName');
-const btnDeleteAll= document.querySelector('#btnDeleteAll');
-const taskList= document.querySelector('#task-list');
+const form = document.querySelector('form');
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.querySelector('#btnDeleteAll');
+const taskList = document.querySelector('#task-list');
 const list = document.querySelector('ul');
 let items;
 
@@ -28,30 +28,30 @@ loadItems();
 //call event listeners
 eventListeners();
 
-function eventListeners(){
+function eventListeners() {
     //add task event
     form.addEventListener('submit', addNewItem);
-    
+
     //delete Items event
     taskList.addEventListener('click', deleteItem);
-    
+
     //delete all items
     btnDeleteAll.addEventListener('click', deleteAllItems);
-    
-    
+
+
     // Completed symbol when clicking on a list item
-    list.addEventListener('click', thisCompleted , false);
+    list.addEventListener('click', thisCompleted, false);
 
 }
 
 //complate item
 function thisCompleted(ev) {
     if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
+        ev.target.classList.toggle('checked');
     }
-  }
+}
 
- // load items 
+// load items 
 function loadItems() {
     items = getItemsFromLS();
     items.forEach(function (item) {
@@ -60,31 +60,31 @@ function loadItems() {
 }
 
 // get items from Local Storage
-function getItemsFromLS(){
-    if(localStorage.getItem('items')===null){
+function getItemsFromLS() {
+    if (localStorage.getItem('items') === null) {
         items = [];
-    }else{
+    } else {
         items = JSON.parse(localStorage.getItem('items'));
     }
     return items;
 }
 
 // set item to Local Storage
-function setItemToLS(text){
+function setItemToLS(text) {
     items = getItemsFromLS();
     items.push(text);
-    localStorage.setItem('items',JSON.stringify(items));
+    localStorage.setItem('items', JSON.stringify(items));
 }
 
 // delete item from Local Storage
-function deleteItemFromLS(text){
+function deleteItemFromLS(text) {
     items = getItemsFromLS();
-    items.forEach(function(item,index){
-        if(item === text){
-            items.splice(index,1);   
+    items.forEach(function (item, index) {
+        if (item === text) {
+            items.splice(index, 1);
         }
     });
-    localStorage.setItem('items',JSON.stringify(items));
+    localStorage.setItem('items', JSON.stringify(items));
 }
 
 // create item
@@ -111,43 +111,43 @@ function createItem(text) {
 
 
 //add new item
-function addNewItem(e){
-    if(input.value ===''){
+function addNewItem(e) {
+    if (input.value === '') {
         alert('Bir görev giriniz.');
     }
 
-     // create item
-     createItem(input.value);
+    // create item
+    createItem(input.value);
 
-     // save to LS
-     setItemToLS(input.value);
- 
-     // clear input
-     input.value = '';
- 
-     e.preventDefault();
+    // save to LS
+    setItemToLS(input.value);
+
+    // clear input
+    input.value = '';
+
+    e.preventDefault();
 
 }
 
 //delete item
-function deleteItem(e){
-    if(e.target.parentElement.classList.contains('delete-item')){
-        if(confirm('Görevi silinsin mi?')){
+function deleteItem(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Görevi silinsin mi?')) {
             e.target.parentElement.parentElement.remove();
 
             // delete item from Local Storage
             deleteItemFromLS(e.target.parentElement.parentElement.textContent);
-        
+
         }
     }
     e.preventDefault();
-        
+
 }
 
 //delete all items
-function deleteAllItems(e){
-    if(confirm('Tüm görevler silinsin mi?')){
-        while(taskList.firstChild){
+function deleteAllItems(e) {
+    if (confirm('Tüm görevler silinsin mi?')) {
+        while (taskList.firstChild) {
             taskList.removeChild(taskList.firstChild);
         }
         localStorage.clear();
