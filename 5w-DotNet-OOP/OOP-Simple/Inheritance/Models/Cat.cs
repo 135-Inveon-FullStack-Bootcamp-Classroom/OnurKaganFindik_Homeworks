ba294f8d57dbd220a6d050cafb2eaaeb0c56a76c
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-11-20 00:55:32 +0300

    Cat class

--- 5w-DotNet-OOP/OOP-Simple/Inheritance/Models/Cat.cs
+++ 5w-DotNet-OOP/OOP-Simple/Inheritance/Models/Cat.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Inheritance.Models
 {
-    class Cat
+    public class Cat : Pet
     {
+        private readonly string[] hobbies = { "Koltuk kenarlarını tırmalamak.", "Top ile oynamak", " Hareketli küçük şeyleri avlamak " };
+        public void Hobby()
+        {
+            Random rnd = new Random();
+            Console.WriteLine($"Hobi: {hobbies[rnd.Next(hobbies.Length)]}");
+        }
+        public override void Speak()
+        {
+
+            string name = GetName() == null ? "Ad belirtilmemiş" : GetName();
+            int life = GetLifeExpentancy();
+
+            Console.WriteLine($"{name}({this.GetType().Name}) isimli hayvan tahmini {life} yıl kadar yaşayabilir.");
+        }
     }
 }
