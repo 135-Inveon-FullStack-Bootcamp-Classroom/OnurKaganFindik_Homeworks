eb926803ce2839ef69d9469c2892ce6db5bf66ac
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-11-20 01:07:49 +0300

    Bird, Cat classes

--- 5w-DotNet-OOP/OOP-Simple/Inheritance/Models/Bird.cs
+++ 5w-DotNet-OOP/OOP-Simple/Inheritance/Models/Bird.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Inheritance.Models
 {
-    class Bird
+    public class Bird : Pet
     {
+
+        public void Species()
+        {
+            string[] species = { " Papağan", "Muhabbet Kuşu", "Hint Bülbülü", "Kanarya" };
+
+            Random rnd = new Random();
+            Console.WriteLine($"Tür: {species[rnd.Next(species.Length)]}");
+        }
+
+        public override void Speak()
+        {
+            string name = GetName() == null ? "Ad belirtilmemiş" : GetName();
+            int life = GetLifeExpentancy();
+
+            Console.WriteLine($"{name}({this.GetType().Name}) isimli hayvan tahmini {life} yıl kadar yaşayabilir.");
+        }
     }
 }
