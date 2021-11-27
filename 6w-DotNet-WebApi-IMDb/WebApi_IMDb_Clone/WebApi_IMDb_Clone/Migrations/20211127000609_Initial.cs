7279463634cacaab11faa9478cd82ba4fc6bf162
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-11-27 03:07:22 +0300

    add initial migration

---
+++ 6w-DotNet-WebApi-IMDb/WebApi_IMDb_Clone/WebApi_IMDb_Clone/Migrations/20211127000609_Initial.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi_IMDb_Clone.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirthDay = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DirectorFullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseDate = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductionName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productions", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "BirthDay", "FullName" },
+                values: new object[,]
+                {
+                    { 1, "01/01/1941", "Full Name 1" },
+                    { 2, "01/01/1941", "Full Name 2" },
+                    { 3, "01/01/1941", "Full Name 3" },
+                    { 4, "01/01/1941", "Full Name 4" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "Category1" },
+                    { 2, "Category2" },
+                    { 3, "Category3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "DirectorFullName", "MovieName", "ReleaseDate" },
+                values: new object[,]
+                {
+                    { 1, "Descrip descrip", "Director Name", "Movie Name 1", "01/01/2021" },
+                    { 2, "Descrip descrip", "Director Name", "Movie Name 2", "01/01/2021" },
+                    { 3, "Descrip descrip", "Director Name", "Movie Name 3", "01/01/2021" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productions",
+                columns: new[] { "Id", "ProductionName" },
+                values: new object[,]
+                {
+                    { 1, "A Company" },
+                    { 2, "B Company" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Productions");
+        }
+    }
+}
